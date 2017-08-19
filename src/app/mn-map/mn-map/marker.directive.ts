aa8b6d9f2488c3fd31efc690f7071126c089a614
Marco Montanari <marco.montanari@gmail.com>
2017-08-19 16:13:20 -0400

    marker magic

--- src/app/mn-map/mn-map/marker.directive.ts
+++ src/app/mn-map/mn-map/marker.directive.ts
@@ -1,5 +1,6 @@
 import { MarkerLayer } from './markerlayer.directive';
-import { Directive, Input, Output, Inject, forwardRef, EventEmitter, ElementRef } from '@angular/core';
+import { Directive, Input, Output, Inject, forwardRef, EventEmitter, ElementRef, Renderer2, AfterContentInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser'
 
 /**
  * Marker for Marker Layer
@@ -8,7 +9,7 @@ import { Directive, Input, Output, Inject, forwardRef, EventEmitter, ElementRef 
 @Directive({
   selector: 'marker',
 })
-export class Marker{
+export class Marker implements AfterContentInit {
   @Input() lon:number;
   @Input() lat:number;
   @Input() icon:string;
@@ -22,32 +23,55 @@ export class Marker{
       this.parent.redraw();
     }
   }
+  
   @Output() datachange = new EventEmitter<any>();
 
-  constructor(@Inject(forwardRef(() => MarkerLayer)) private parent:MarkerLayer, private elt:ElementRef){}
-
+  //constructor(@Inject(forwardRef(() => MarkerLayer)) private parent:MarkerLayer, private elt:ElementRef){}
+  constructor(@Inject(forwardRef(() => MarkerLayer)) private parent:MarkerLayer, private elt:ElementRef, private sanitizer: DomSanitizer, private rd: Renderer2){}
+  
   public addMarker(lyr){
-
     let m = this.getMarker();
     if (m != null){
       lyr.addLayer(m);
     }
   }
   
-  public getMarker(){
+  public ngAfterContentInit(){
+    let content = this.elt.nativeElement;
+    let pop = "<div></div>";
+    let sanepop = this.sanitizer.bypassSecurityTrustHtml(this.elt.nativeElement);
+    console.log(sanepop["changingThisBreaksApplicationSecurity"].innerHtml);  
+    if (!this.marker)
+      this.prepareMarker();
+    //this.rd.setProperty(this.marker["_popup"], "_content", content.innerHtml)
+    
+    this.marker["_popup"].setContent("<div>"+content.innerHtml+"</div>");
+  }
+
+  private marker;
+
+  private prepareMarker(){
     if (this.data == null){
-      if (this.lat !== undefined)
-        return L.marker([this.lat, this.lon]);
+      if (this.lat !== undefined){
+        this.marker = L.marker([this.lat, this.lon]);
+        this.marker.bindPopup("").openPopup();
+        return this.marker;
+      }
       else return null;
     } else {
       if (this.data.geometry) {
         if (this.data.geometry.coordinates[0] != 0) {
-          let pop = "<div>"+this.elt.nativeElement.innerHtml+"</div>";
-          let mrk = L.marker(this.data.geometry.coordinates);
-          mrk.bindPopup(pop).openPopup();
-          return mrk;
+          this.marker = L.marker(this.data.geometry.coordinates);
+          this.marker.bindPopup("").openPopup();
+          return this.marker;
         }
       }
     }
+  }
+
+  public getMarker(){
+    if (!this.marker)
+      this.prepareMarker();
+    return this.marker;
   }
 } 
